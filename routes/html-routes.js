e0b5717312d78e0ade7c044445a8af11d2d1640b
Zayd Sarraj <zayd.sarraj@gmail.com>
2020-02-22 11:47:59 -0500

    added seeds, required db in routes file, fixed server to post to index

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,18 +1,13 @@
 module.exports = function(app) {
 
 
-    app.get("/", function(req, res) {
-        // Set posts variable = data in the table
-        // Maybe reset certain obj keys, as in 'captions.all where votes = highest' and set that as TopPost
-        posts.all(function(data) {
-            var FrontPagePosts = {
-              posts: data
-            };
-            res.render("index", FrontPagePosts);
-          });
-      res.render(path.join(__dirname, "../public/blog.html"));
-    });
-  
-  
-  };
-  
+  app.get("/", function(req, res) {
+    // Set posts variable = data in the table
+    // Maybe reset certain obj keys, as in 'captions.all where votes = highest' and set that as TopPost
+    Posts.findAll({}).then(function(data) {
+      
+        res.render("index", data);
+      });
+});
+
+};
